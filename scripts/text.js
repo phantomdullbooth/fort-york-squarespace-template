8155b75c1dfa1627fb4240e5ae56982dd775b72e
Addison <49768250+winslowdev@users.noreply.github.com>
2020-09-18 01:50:39 -0400

    styling individual section blocks

--- scripts/text.js
+++ scripts/text.js
@@ -1,16 +1,10 @@
 function postmarkArticle(location, publishedOn, mainCategory) {
-    const basicInformation = document.getElementById('unimportant')
-    const information = document.getElementById('unimportant-main')
+    const postmarkLocation = document.getElementById(location)
     const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
     const publishedDate = new Date(parseInt(publishedOn))
-
 
     let month = months[publishedDate.getMonth()]
     let date = publishedDate.getDate()
 
-    if (location === 'main') {
-        information.innerHTML = month + ' ' + date + ' —— ' + mainCategory
-    } else {
-        basicInformation.innerHTML = month + ' ' + date + ' —— ' + mainCategory
-    }
+    postmarkLocation.innerHTML = month + ' ' + date + ' —— ' + mainCategory
 }
