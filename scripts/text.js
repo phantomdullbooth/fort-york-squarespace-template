95cd15c90f6134e2318013d1e5546d23e6d21393
Addison <49768250+winslowdev@users.noreply.github.com>
2020-09-18 16:14:56 -0400

    added author signature

--- scripts/text.js
+++ scripts/text.js
@@ -67,3 +67,57 @@ function renderSneakpeek(articleType, graphicBox, excerptBox) {
             </div>
         {.end}
     </squarespace:query> */}
+
+
+{/* <div class="page page--blogs">
+    <squarespace:block-field id="blog-header" columns="1" />
+
+    <div>
+        {.repeated section items}
+            <div class="card card--blog">
+                <a href="{fullUrl}">
+                    <div class="container">
+                        <h1>{title}</h1>
+                        <p>{excerpt}</p>
+                    </div>
+
+                    <button><img src="{website.internalUrl}/assets/icons/arrow.png" /></button>
+                </a>
+            </div>
+        {.end}
+    </div>
+</div>
+
+<script>
+    function greetBlogUser() {
+        const blogHeader = document.getElementById("blog-header")
+        const blogGreeting = blogHeader.getElementsByTagName("H1")[0]
+        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+        const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+        const time = new Date()
+
+        let month = months[time.getMonth()]
+        let day = days[time.getDay()]
+        let date = time.getDate()
+        let hour = time.getHours()
+
+        if (month === "December" && date === 25) {
+            blogGreeting.innerHTML = "Merry Christmas!"
+        } else if (month === "January" && date === 1) {
+            blogGreeting.innerHTML = "Happy New Year!"
+        } else if (month === "July" && date === 4) {
+            blogGreeting.innerHTML = "Happy 4th of July!"
+        } else if (day === "Friday") {
+            blogGreeting.innerHTML = "Thank God it's Friday!"
+        } else if (hour >= 6 && hour < 12) {
+            blogGreeting.innerHTML = "good morning."
+        } else if (hour >= 12 && hour < 17) {
+            blogGreeting.innerHTML = "good afternoon."
+        } else {
+            blogGreeting.innerHTML = "good evening."
+        }
+    }
+
+    greetBlogUser()
+</script> */}
