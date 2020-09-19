6b78e006bf46351b825de1b3f24eb0c09cc507cf
Addison <49768250+winslowdev@users.noreply.github.com>
2020-09-18 22:24:14 -0400

    creating menu

--- scripts/style.js
+++ scripts/style.js
@@ -4,11 +4,15 @@ const navIcon = document.getElementById("nav-icon")
 function colorHeaderElements(navigationOpen) {
     if (navigationOpen === true) {
        headerBackground.style.backgroundColor = '#000000'
+    } else {
+        headerBackground.style.backgroundColor = 'none'
     }
 }
 
 function toggleNavigation() {
     if (headerBackground.style.backgroundColor !== '#000000') {
+        colorHeaderElements(true)
+    } else {
         colorHeaderElements(true)
     }
 }
