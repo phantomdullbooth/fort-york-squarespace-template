a422d7ada03e553430a8edaee143770cabc18831
Addison <49768250+winslowdev@users.noreply.github.com>
2020-09-18 23:01:56 -0400

    fixing aEL double-click

--- scripts/style.js
+++ scripts/style.js
@@ -1,7 +1,6 @@
 const headerBackground = document.getElementById("header")
 const navIcon = document.getElementById("nav-icon")
 
-
 function colorHeaderElements(navigationOpen) {
     if (navigationOpen === true) {
        headerBackground.style.backgroundColor = '#000000'
