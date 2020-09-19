0394e1d2c870b8fcb535ef07fa1024aecd5e6aed
Addison <49768250+winslowdev@users.noreply.github.com>
2020-09-18 22:23:28 -0400

    creating menu

--- scripts/style.js
+++ scripts/style.js
@@ -2,7 +2,7 @@ const headerBackground = document.getElementById("header")
 const navIcon = document.getElementById("nav-icon")
 
 function colorHeaderElements(navigationOpen) {
-    if (!navigationOpen) {
+    if (navigationOpen === true) {
        headerBackground.style.backgroundColor = '#000000'
     }
 }
